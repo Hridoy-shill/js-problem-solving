8743fa989de0e03b28c5a3478f6b9487939a8471
Hridoy-shill <ridoyaaa60@gmail.com>
2023-01-29 18:19:38 +0600

    problem8 solved

---
+++ problem8.js
@@ -0,0 +1,9 @@
+var side1 = 26;
+var side2 = 26;
+var side3 = 6;
+if(side1 == side2 || side2 == side3 || side1 == side3){
+    console.log('yes it is isosceles');
+}
+else{
+    console.log('no it is not isosceles');
+}
