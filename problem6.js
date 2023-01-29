15b7540a4ef9823968edadf2f53fef10b2345706
Hridoy-shill <ridoyaaa60@gmail.com>
2023-01-29 13:26:20 +0600

    problem-6 solved

---
+++ problem6.js
@@ -0,0 +1,27 @@
+// var num1 = 341;
+// var num2 = 40;
+// var num3 = 745;
+// if(num1>num2){
+//     console.log('number 1 is big');
+// }
+// else if(num2>num3){
+//     console.log('yes number 2 is big');
+// }
+// else{
+//     console.log('number 3 is big');
+// }
+var num1 = 825;
+var num2 = 997;
+var num3 = 1186;
+if(num1>num2){
+    console.log('num-1');
+}
+else if(num2>num3){
+    console.log('num-2');
+}
+else if(num1>num3){
+    console.log('num-1');
+}
+else{
+    console.log('num-3');
+}
