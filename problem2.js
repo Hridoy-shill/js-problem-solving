8a798b3a7e1f2497a0035bb4fc52305b102091de
Hridoy-shill <ridoyaaa60@gmail.com>
2023-01-29 11:38:52 +0600

    problem 2 solved

---
+++ problem2.js
@@ -0,0 +1,8 @@
+var math = 75.25;
+var bio = 65;
+var chms = 80;
+var phy = 35.45;
+var bng = 99.50;
+var totalMark = math+bio+chms+phy+bng
+var totalAvg = totalMark / 5
+console.log(totalAvg.toFixed(2));
