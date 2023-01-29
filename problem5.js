7c1106f9dee6af931748e4a401164a17c575c525
Hridoy-shill <ridoyaaa60@gmail.com>
2023-01-29 12:36:41 +0600

    problem5 solved

---
+++ problem5.js
@@ -0,0 +1,8 @@
+var fruits=['Appel', 'Banana', 'Orange']
+// console.log(fruits.indexOf('Banana'));
+fruits[1]='Mango';
+// console.log(fruits);
+fruits.pop();
+// console.log(fruits);
+fruits.push('Watermelon')
+console.log(fruits);
